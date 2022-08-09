a04e89f2a42d9ef72e16b593bdaae6c570b3c495
Per Odlund <per.odlund@gmail.com>
2022-08-10 00:31:59 +0200

    Get the index and total for message searches

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Cow, io};
+use std::{borrow::Cow, cmp::Ordering, io};
 use std::{error, fmt, result};
 extern crate home;
 extern crate notmuch;
@@ -17,6 +17,7 @@ type Result<T> = result::Result<T, Error>;
 enum Error {
     SerdeErr(serde_json::Error),
     NmError(notmuch::Error),
+    IoError(io::Error),
 }
 
 impl fmt::Display for Error {
@@ -24,6 +25,7 @@ impl fmt::Display for Error {
         match self {
             Error::SerdeErr(e) => <serde_json::Error as fmt::Display>::fmt(e, f),
             Error::NmError(e) => <notmuch::Error as fmt::Display>::fmt(e, f),
+            Error::IoError(e) => <io::Error as fmt::Display>::fmt(e,f),
         }
     }
 }
@@ -33,7 +35,14 @@ impl error::Error for Error {
         match &self {
             Error::SerdeErr(e) => Some(e),
             Error::NmError(e) => Some(e),
+            Error::IoError(e) => Some(e),
         }
+    }
+}
+
+impl std::convert::From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::IoError(err)
     }
 }
 
@@ -70,9 +79,9 @@ impl<'a> Serialize for Thread<'a> {
     }
 }
 
-struct Message<'a>(&'a notmuch::Message);
+struct Message(notmuch::Message, i32, i32);
 
-impl<'a> Serialize for Message<'a> {
+impl Serialize for Message {
     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
     where
         S: serde::Serializer {
@@ -85,13 +94,21 @@ impl<'a> Serialize for Message<'a> {
             let filename = self.0.filename();
             mes.serialize_field("filename", &filename)?;
             let filenames: Vec<PathBuf> = self.0.filenames().collect();
-            mes.serialize_field("filnames", &filenames)?;
+            mes.serialize_field("filenames", &filenames)?;
             let tags: Vec<String> = self.0.tags().collect();
             mes.serialize_field("tags", &tags)?;
             let from = self.0.header("From").map_err(Error::custom)?.unwrap_or_else(|| Cow::from(""));
             mes.serialize_field("from", &from)?;
             let subject = self.0.header("Subject").map_err(Error::custom)?.unwrap_or_else(|| Cow::from(""));
             mes.serialize_field("subject", &subject)?;
+            let tid = self.0.thread_id();
+            mes.serialize_field("tid", &tid)?;
+            let index = self.1;
+            mes.serialize_field("index", &index)?;
+            let total = self.2;
+            mes.serialize_field("total", &total)?;
+            let keys : Vec<(String, String)> =  self.0.properties("session-key", true).collect();
+            mes.serialize_field("keys", &keys)?;
             mes.end()
     }
 }
@@ -106,10 +123,9 @@ fn from_str(s: &str) -> Sort {
         }
 }
 
-fn show_message<W>(message: &notmuch::Message, writer: &mut W) -> Result<()> 
+fn show_message<W>(message: &Message, writer: &mut W) -> Result<()> 
     where W: io::Write {
-    let ser = Message(message);
-    serde_json::to_writer(writer, &ser)?;
+    serde_json::to_writer(writer, message)?;
     Ok(())
 }
 
@@ -120,13 +136,42 @@ fn show_thread<W>(thread: &notmuch::Thread, writer: &mut W) -> Result<()>
     Ok(())
 }
 
+fn sort_oldest(a: &Message, b: &Message) -> Ordering {
+    let adate = a.0.date();
+    let bdate = b.0.date();
+    adate.cmp(&bdate)
+}
+fn sort_newest(a: &Message, b: &Message) -> Ordering {
+    let adate = a.0.date();
+    let bdate = b.0.date();
+    bdate.cmp(&adate)
+}
+
 fn show_messages<W>(db: &notmuch::Database, sort: Sort, str: &str, writer: &mut W) -> Result<()>
     where W: io::Write {
     let query = db.create_query(str)?;
-    query.set_sort(sort);
-    let messages = query.search_messages()?;
-    for message in messages {
+    let threads = query.search_threads()?;
+    let mut matched_messages = Vec::new();
+    for thread in threads {
+        let mut index = 1;
+        let total = thread.total_messages();
+        for message in thread.messages() {
+            let matched = message.get_flag(notmuch::MessageFlag::Match);
+            if matched {
+                let ser = Message(message.clone(), index, total);
+                matched_messages.push(ser);
+            }
+            index=index+1
+        }
+    }
+    match sort {
+        Sort::OldestFirst => matched_messages.sort_by(sort_oldest),
+        Sort::NewestFirst => matched_messages.sort_by(sort_newest),
+        _ => panic!("This shouldn't happen")
+    }
+    for message in matched_messages.iter() {
         show_message(&message, writer)?;
+        write!(writer, "\n")?;
     }
     Ok(())
 }
@@ -138,47 +183,64 @@ fn show_threads<W>(db: &notmuch::Database, sort: Sort, str: &str, writer: &mut W
     let threads = query.search_threads()?;
     for thread in threads {
         show_thread(&thread, writer)?;
+        write!(writer, "\n")?;
     }
     Ok(())
 }
 
 fn show_before_message<W>(db: &notmuch::Database, id: &str, filter: &Option<&str>, writer: &mut W) -> Result<()>
     where W: io::Write {
-    let mut query = format!("thread:{{mid:{}}}", id);
+    let mut query = format!("mid:{}", id);
     if let Some(str) = filter {
         query.push_str(" and ");
         query.push_str(str);
     }
-    let q = db.create_query(&query)?;
-    let messages = q.search_messages()?;
-    for message in messages {
-        if message.id() == id {
-            break;
+    let query = db.create_query(&query)?;
+    let threads = query.search_threads()?;
+    for thread in threads {
+        let mut index = 1;
+        let total = thread.total_messages();
+        for message in thread.messages() {
+            if message.id() == id {
+                break;
+            }
+            let ser = Message(message.clone(), index, total);
+            show_message(&ser, writer)?;
+            write!(writer, "\n")?;
+            index=index+1
         }
-        show_message(&message, writer)?;
+        break; // there should only be one thread
     }
     Ok(())
 }
 
 fn show_after_message<W>(db: &notmuch::Database, id: &str, filter: &Option<&str>, writer: &mut W) -> Result<()>
     where W: io::Write {
-    let mut query = format!("thread:{{mid:{}}}", id);
+    let mut query = format!("mid:{}", id);
     if let Some(str) = filter {
         query.push_str(" and ");
         query.push_str(str);
     }
-    let q = db.create_query(&query)?;
-    let messages = q.search_messages()?;
-    let mut seen = false;
-    for message in messages {
-        if message.id() == id {
-            seen = true;
-            continue;
+    let query = db.create_query(&query)?;
+    let threads = query.search_threads()?;
+    for thread in threads {
+        let mut index = 0;
+        let mut seen = false;
+        let total = thread.total_messages();
+        for message in thread.messages() {
+            index=index+1;
+            if message.id() == id {
+                seen = true;
+                continue;
+            }
+            if !seen {
+                continue;
+            }
+            let ser = Message(message.clone(), index, total);
+            show_message(&ser, writer)?;
+            write!(writer, "\n")?;
         }
-        if !seen {
-            continue;
-        }
-        show_message(&message, writer)?;
+        break; // there should only be one thread
     }
     Ok(())
 }
@@ -187,11 +249,17 @@ fn show_after_message<W>(db: &notmuch::Database, id: &str, filter: &Option<&str>
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     #[clap(short, long, value_name = "oldest|newest")]
-    #[clap(default_value_t = String::from("oldest"))]
+    #[clap(default_value_t = String::from("newest"))]
     sort: String,
 
     #[clap(short, long, parse(from_os_str), value_name = "FILE")]
     db_path: Option<PathBuf>,
+
+    #[clap(short, long, parse(from_os_str), value_name = "FILE")]
+    conf_path: Option<PathBuf>,
+
+    #[clap(short, long)]
+    profile: Option<String>,
 
     #[clap(subcommand)]
     command: Commands,
@@ -221,7 +289,7 @@ enum Commands {
 fn main() -> Result<()>{
     let args = Cli::parse();
 
-    let db = notmuch::Database::open(args.db_path, notmuch::DatabaseMode::ReadOnly)?;
+    let db = notmuch::Database::open_with_config(args.db_path, notmuch::DatabaseMode::ReadOnly, args.conf_path, args.profile.as_deref())?;
     let sort = from_str(&args.sort);
     let mut writer = std::io::BufWriter::new(io::stdout().lock());
 
